68c121d5dc19d299b5ea606293726368347e9623
João Pedro Marchiori Santos <128313369+Arrowbr28@users.noreply.github.com>
2024-04-25 11:22:38 -0300

    Calma ai rapaz estamos começando ai! csharp

---
+++ Aplicativodeconsolejp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace teste
+{
+class principalminha
+{
+    int numero = 19;
+    String nome = "joaopedro"; 
+    
+    void funcaoai()
+        {
+            String peganome = Console.ReadLine("Digita o seu nome e vamos repetir 10 vezes ele");
+            for(int a = 0; )
+            {
+                int exibe += 
+            }
+        }
+
+    public static void main(String args[])
+    {
+
+    }
+}
+}
+
